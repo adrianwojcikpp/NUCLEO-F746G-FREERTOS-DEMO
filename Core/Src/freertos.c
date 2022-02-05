070571cd46ba4b25d8657a980a823c8611218321
adrianwojcikpp <54798218+adrianwojcikpp@users.noreply.github.com>
2022-02-05 02:02:47 +0100

    Initial refactor

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -21,13 +21,19 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "usart.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
+typedef StaticTask_t osStaticThreadDef_t;
+typedef StaticQueue_t osStaticMessageQDef_t;
+typedef StaticSemaphore_t osStaticMutexDef_t;
+typedef StaticSemaphore_t osStaticSemaphoreDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
@@ -46,11 +52,207 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 2048 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Sender_task */
+osThreadId_t Sender_taskHandle;
+const osThreadAttr_t Sender_task_attributes = {
+  .name = "Sender_task",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
+};
+/* Definitions for Reciver_task */
+osThreadId_t Reciver_taskHandle;
+uint32_t Reciver_taskBuffer[ 2048 ];
+osStaticThreadDef_t Reciver_taskControlBlock;
+const osThreadAttr_t Reciver_task_attributes = {
+  .name = "Reciver_task",
+  .cb_mem = &Reciver_taskControlBlock,
+  .cb_size = sizeof(Reciver_taskControlBlock),
+  .stack_mem = &Reciver_taskBuffer[0],
+  .stack_size = sizeof(Reciver_taskBuffer),
+  .priority = (osPriority_t) osPriorityRealtime,
+};
+/* Definitions for Send_Sensor_Data */
+osMessageQueueId_t Send_Sensor_DataHandle;
+uint8_t Send_Sensor_DataBuffer[ 16 * 4 ];
+osStaticMessageQDef_t Send_Sensor_DataControlBlock;
+const osMessageQueueAttr_t Send_Sensor_Data_attributes = {
+  .name = "Send_Sensor_Data",
+  .cb_mem = &Send_Sensor_DataControlBlock,
+  .cb_size = sizeof(Send_Sensor_DataControlBlock),
+  .mq_mem = &Send_Sensor_DataBuffer,
+  .mq_size = sizeof(Send_Sensor_DataBuffer)
+};
+/* Definitions for Check_IMU_Angle_Timer */
+osTimerId_t Check_IMU_Angle_TimerHandle;
+const osTimerAttr_t Check_IMU_Angle_Timer_attributes = {
+  .name = "Check_IMU_Angle_Timer"
+};
+/* Definitions for USART_MUTEX */
+osMutexId_t USART_MUTEXHandle;
+osStaticMutexDef_t USART_MUTEXControlBlock;
+const osMutexAttr_t USART_MUTEX_attributes = {
+  .name = "USART_MUTEX",
+  .cb_mem = &USART_MUTEXControlBlock,
+  .cb_size = sizeof(USART_MUTEXControlBlock),
+};
+/* Definitions for Motion_Semaphore */
+osSemaphoreId_t Motion_SemaphoreHandle;
+osStaticSemaphoreDef_t Motion_Semaphore_ControlBlock;
+const osSemaphoreAttr_t Motion_Semaphore_attributes = {
+  .name = "Motion_Semaphore",
+  .cb_mem = &Motion_Semaphore_ControlBlock,
+  .cb_size = sizeof(Motion_Semaphore_ControlBlock),
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
 /* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+extern void Start_Sender_task(void *argument);
+void Start_Reciver_task(void *argument);
+extern void Check_Angle(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+  /* Create the mutex(es) */
+  /* creation of USART_MUTEX */
+  USART_MUTEXHandle = osMutexNew(&USART_MUTEX_attributes);
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* Create the semaphores(s) */
+  /* creation of Motion_Semaphore */
+  Motion_SemaphoreHandle = osSemaphoreNew(1, 1, &Motion_Semaphore_attributes);
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* Create the timer(s) */
+  /* creation of Check_IMU_Angle_Timer */
+  Check_IMU_Angle_TimerHandle = osTimerNew(Check_Angle, osTimerOnce, NULL, &Check_IMU_Angle_Timer_attributes);
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of Send_Sensor_Data */
+  Send_Sensor_DataHandle = osMessageQueueNew (16, 4, &Send_Sensor_Data_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  //defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of Sender_task */
+  Sender_taskHandle = osThreadNew(Start_Sender_task, NULL, &Sender_task_attributes);
+
+  /* creation of Reciver_task */
+  Reciver_taskHandle = osThreadNew(Start_Reciver_task, NULL, &Reciver_task_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  char buff[200];
+
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(200);
+
+    // To make sure that whole text is send in one go
+    osMutexAcquire(USART_MUTEXHandle, osWaitForever);
+
+    // some long action
+    for(uint16_t i = 0; i<100; i++)
+    {
+      uint16_t len =  (uint16_t)sprintf(buff, "Some long text %d ", i);
+      HAL_UART_Transmit(&huart3, (uint8_t*)buff, len, 10);
+	}
+
+    HAL_UART_Transmit(&huart3, (uint8_t*)"\n", (uint8_t)2, 10);
+
+    osMutexRelease(USART_MUTEXHandle);
+
+    HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_Start_Reciver_task */
+/**
+* @brief Function implementing the Reciver_task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_Start_Reciver_task */
+void Start_Reciver_task(void *argument)
+{
+  /* USER CODE BEGIN Start_Reciver_task */
+  char buff[64];
+
+  /* Infinite loop */
+  for(;;)
+  {
+    uint16_t recived_data[2];
+    osStatus_t status = osMessageQueueGet(Send_Sensor_DataHandle, &recived_data[0], NULL, osWaitForever);
+
+    if (status == osOK)
+    {
+      osMutexAcquire(USART_MUTEXHandle, osWaitForever);
+	  uint16_t len = (uint16_t)sprintf(buff, "Roll angle = %d\r Pitch angle = %d\r", recived_data[0], recived_data[1]);
+	  HAL_UART_Transmit(&huart3, (uint8_t*)buff, len, 10);
+	  osMutexRelease(USART_MUTEXHandle);
+	}
+    else
+    {
+      Error_Handler();
+    }
+  }
+  /* USER CODE END Start_Reciver_task */
+}
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,9 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -27,10 +30,6 @@
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
-typedef StaticTask_t osStaticThreadDef_t;
-typedef StaticQueue_t osStaticMessageQDef_t;
-typedef StaticSemaphore_t osStaticMutexDef_t;
-typedef StaticSemaphore_t osStaticSemaphoreDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
@@ -47,85 +46,13 @@ uint32_t counter_disp;
 
 /* Private variables ---------------------------------------------------------*/
 
-I2C_HandleTypeDef hi2c1;
-
-UART_HandleTypeDef huart3;
-
-PCD_HandleTypeDef hpcd_USB_OTG_FS;
-
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 2048 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
-/* Definitions for Sender_task */
-osThreadId_t Sender_taskHandle;
-const osThreadAttr_t Sender_task_attributes = {
-  .name = "Sender_task",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityHigh,
-};
-/* Definitions for Reciver_task */
-osThreadId_t Reciver_taskHandle;
-uint32_t Reciver_taskBuffer[ 2048 ];
-osStaticThreadDef_t Reciver_taskControlBlock;
-const osThreadAttr_t Reciver_task_attributes = {
-  .name = "Reciver_task",
-  .cb_mem = &Reciver_taskControlBlock,
-  .cb_size = sizeof(Reciver_taskControlBlock),
-  .stack_mem = &Reciver_taskBuffer[0],
-  .stack_size = sizeof(Reciver_taskBuffer),
-  .priority = (osPriority_t) osPriorityRealtime,
-};
-/* Definitions for Send_Sensor_Data */
-osMessageQueueId_t Send_Sensor_DataHandle;
-uint8_t Send_Sensor_DataBuffer[ 16 * 2 * sizeof( uint16_t ) ];
-osStaticMessageQDef_t Send_Sensor_DataControlBlock;
-const osMessageQueueAttr_t Send_Sensor_Data_attributes = {
-  .name = "Send_Sensor_Data",
-  .cb_mem = &Send_Sensor_DataControlBlock,
-  .cb_size = sizeof(Send_Sensor_DataControlBlock),
-  .mq_mem = &Send_Sensor_DataBuffer,
-  .mq_size = sizeof(Send_Sensor_DataBuffer)
-};
-/* Definitions for Check_IMU_Angle_Timer */
-osTimerId_t Check_IMU_Angle_TimerHandle;
-const osTimerAttr_t Check_IMU_Angle_Timer_attributes = {
-  .name = "Check_IMU_Angle_Timer"
-};
-/* Definitions for USART_MUTEX */
-osMutexId_t USART_MUTEXHandle;
-osStaticMutexDef_t USART_MUTEXControlBlock;
-const osMutexAttr_t USART_MUTEX_attributes = {
-  .name = "USART_MUTEX",
-  .cb_mem = &USART_MUTEXControlBlock,
-  .cb_size = sizeof(USART_MUTEXControlBlock),
-};
-/* Definitions for Motion_Semaphore */
-osSemaphoreId_t Motion_SemaphoreHandle;
-osStaticSemaphoreDef_t Motion_Semaphore_ControlBlock;
-const osSemaphoreAttr_t Motion_Semaphore_attributes = {
-  .name = "Motion_Semaphore",
-  .cb_mem = &Motion_Semaphore_ControlBlock,
-  .cb_size = sizeof(Motion_Semaphore_ControlBlock),
-};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART3_UART_Init(void);
-static void MX_USB_OTG_FS_PCD_Init(void);
-static void MX_I2C1_Init(void);
-void StartDefaultTask(void *argument);
-extern void Start_Sender_task(void *argument);
-void Start_Reciver_task(void *argument);
-extern void Check_Angle(void *argument);
-
+void MX_FREERTOS_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -164,64 +91,14 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Init scheduler */
-  osKernelInitialize();
-  /* Create the mutex(es) */
-  /* creation of USART_MUTEX */
-  USART_MUTEXHandle = osMutexNew(&USART_MUTEX_attributes);
-
-  /* USER CODE BEGIN RTOS_MUTEX */
-	/* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
-
-  /* Create the semaphores(s) */
-  /* creation of Motion_Semaphore */
-  Motion_SemaphoreHandle = osSemaphoreNew(1, 1, &Motion_Semaphore_attributes);
-
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-	/* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
-
-  /* Create the timer(s) */
-  /* creation of Check_IMU_Angle_Timer */
-  Check_IMU_Angle_TimerHandle = osTimerNew(Check_Angle, osTimerOnce, NULL, &Check_IMU_Angle_Timer_attributes);
-
-  /* USER CODE BEGIN RTOS_TIMERS */
-	/* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
-
-  /* Create the queue(s) */
-  /* creation of Send_Sensor_Data */
-  Send_Sensor_DataHandle = osMessageQueueNew (16, 2*sizeof(uint16_t), &Send_Sensor_Data_attributes);
-
-  /* USER CODE BEGIN RTOS_QUEUES */
-	/* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
-
-  /* Create the thread(s) */
-  /* creation of defaultTask */
-  //defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-
-  /* creation of Sender_task */
-  Sender_taskHandle = osThreadNew(Start_Sender_task, NULL, &Sender_task_attributes);
-
-  /* creation of Reciver_task */
-  Reciver_taskHandle = osThreadNew(Start_Reciver_task, NULL, &Reciver_task_attributes);
-
-  /* USER CODE BEGIN RTOS_THREADS */
-	/* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
-
-  /* USER CODE BEGIN RTOS_EVENTS */
-	/* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
-
+  osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
+  MX_FREERTOS_Init();
   /* Start scheduler */
   osKernelStart();
 
@@ -289,297 +166,8 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x00501E63;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure Analogue filter
-  */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_DISABLE) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure Digital filter
-  */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART3_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART3_Init 0 */
-
-  /* USER CODE END USART3_Init 0 */
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 115200;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
-}
-
-/**
-  * @brief USB_OTG_FS Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USB_OTG_FS_PCD_Init(void)
-{
-
-  /* USER CODE BEGIN USB_OTG_FS_Init 0 */
-
-  /* USER CODE END USB_OTG_FS_Init 0 */
-
-  /* USER CODE BEGIN USB_OTG_FS_Init 1 */
-
-  /* USER CODE END USB_OTG_FS_Init 1 */
-  hpcd_USB_OTG_FS.Instance = USB_OTG_FS;
-  hpcd_USB_OTG_FS.Init.dev_endpoints = 6;
-  hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
-  hpcd_USB_OTG_FS.Init.dma_enable = DISABLE;
-  hpcd_USB_OTG_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
-  hpcd_USB_OTG_FS.Init.Sof_enable = ENABLE;
-  hpcd_USB_OTG_FS.Init.low_power_enable = DISABLE;
-  hpcd_USB_OTG_FS.Init.lpm_enable = DISABLE;
-  hpcd_USB_OTG_FS.Init.vbus_sensing_enable = ENABLE;
-  hpcd_USB_OTG_FS.Init.use_dedicated_ep1 = DISABLE;
-  if (HAL_PCD_Init(&hpcd_USB_OTG_FS) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USB_OTG_FS_Init 2 */
-
-  /* USER CODE END USB_OTG_FS_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : USER_Btn_Pin */
-  GPIO_InitStruct.Pin = USER_Btn_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : RMII_MDC_Pin RMII_RXD0_Pin RMII_RXD1_Pin */
-  GPIO_InitStruct.Pin = RMII_MDC_Pin|RMII_RXD0_Pin|RMII_RXD1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : RMII_REF_CLK_Pin RMII_MDIO_Pin RMII_CRS_DV_Pin */
-  GPIO_InitStruct.Pin = RMII_REF_CLK_Pin|RMII_MDIO_Pin|RMII_CRS_DV_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : IMU_INT_Pin */
-  GPIO_InitStruct.Pin = IMU_INT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(IMU_INT_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
-  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : RMII_TXD1_Pin */
-  GPIO_InitStruct.Pin = RMII_TXD1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
-  HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
-  GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : USB_OverCurrent_Pin */
-  GPIO_InitStruct.Pin = USB_OverCurrent_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : RMII_TX_EN_Pin RMII_TXD0_Pin */
-  GPIO_InitStruct.Pin = RMII_TX_EN_Pin|RMII_TXD0_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
-  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 5, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-
-}
-
 /* USER CODE BEGIN 4 */
 /* USER CODE END 4 */
-
-/* USER CODE BEGIN Header_StartDefaultTask */
-/**
- * @brief  Function implementing the defaultTask thread.
- * @param  argument: Not used
- * @retval None
- */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-  /* USER CODE BEGIN 5 */
-
-	char buff[200];
-
-	/* Infinite loop */
-	for(;;)
-	{
-
-		osDelay(200);
-
-
-		// To make sure that whole text is send in one go
-		osMutexAcquire(USART_MUTEXHandle, osWaitForever);
-
-		// some long action
-		for(uint16_t i = 0; i<100; i++){
-			uint16_t len =  (uint16_t)sprintf(buff, "Some long text %d ", i);
-			HAL_UART_Transmit(&huart3, (uint8_t*)buff, len, 10);
-		}
-		HAL_UART_Transmit(&huart3, (uint8_t*)"\n", (uint8_t)2, 10);
-
-
-		osMutexRelease(USART_MUTEXHandle);
-
-
-
-		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
-
-	}
-  /* USER CODE END 5 */
-}
-
-/* USER CODE BEGIN Header_Start_Reciver_task */
-/**
- * @brief Function implementing the Reciver_task thread.
- * @param argument: Not used
- * @retval None
- */
-/* USER CODE END Header_Start_Reciver_task */
-void Start_Reciver_task(void *argument)
-{
-  /* USER CODE BEGIN Start_Reciver_task */
-
-	char buff[30];
-
-	/* Infinite loop */
-	for(;;)
-	{
-		uint16_t recived_data[2];;
-		osStatus_t status = osMessageQueueGet(Send_Sensor_DataHandle, &recived_data[0], NULL, osWaitForever);
-
-		if (status == osOK) {
-
-			osMutexAcquire(USART_MUTEXHandle, osWaitForever);
-			uint16_t len = (uint16_t)sprintf(buff, "Roll angle = %d\r\nPitch angle = %d\r\n" , recived_data[0], recived_data[1]);
-			HAL_UART_Transmit(&huart3, (uint8_t*)buff, len, 10);
-			osMutexRelease(USART_MUTEXHandle);
-
-		}
-		else {
-			Error_Handler();
-		}
-
-
-	}
-  /* USER CODE END Start_Reciver_task */
-}
 
 /**
   * @brief  Period elapsed callback in non blocking mode
